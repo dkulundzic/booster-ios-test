83ff0bef5d18461323c85a679fe8a12ee79f19e1
Domagoj Kulundžić <domagoj.kulundzic.business@gmail.com>
2021-11-01 21:40:45 +0100

    * Updated the copyright.

--- BoosterTest/Networking/Resource/Resource.swift
+++ BoosterTest/Networking/Resource/Resource.swift
@@ -3,7 +3,11 @@
 //  Networking
 //
 //  Created by Luka Šarčević on 06.01.2021..
+<<<<<<< Updated upstream
 //  Copyright © 2021 Libera la Musica Oy. All rights reserved.
+=======
+//  Copyright © 2021 Codeopolis. All rights reserved.
+>>>>>>> Stashed changes
 //
 
 import Foundation
